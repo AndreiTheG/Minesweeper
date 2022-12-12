4e7d55e78a5a30d0fd0943cfaee247115f21c16d
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2022-12-12 17:17:48 +0200

    24th Commit

--- script.js
+++ script.js
@@ -40,7 +40,6 @@ function pressCell() {
             }
         } 
     }
-    console.log(minesCounter);
     for (let i = 0; i < 9; ++i) {
         for (let j = 0; j < 9; ++j) {
             if (matrix[i][j] == 0) {
