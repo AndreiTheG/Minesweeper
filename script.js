afd95329b58f0793fb927a0519383677d1b7e8a7
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2022-12-22 13:24:41 +0200

    148th Commit

--- script.js
+++ script.js
@@ -99,59 +99,31 @@ function neighboursValues(matrix, k, l) {
 }
 
 function visitNeighbours(matrix, row, column) {
-    let firstPos1 = row, firstPos2 = column, lastPos1 = 8, lastPos2 = 8, value1 = 1, value2 = 1;
-    for (let step = 1; step <= 8; ++step) {
+    let lastRow = 8, lastCol = 8, value1 = 1, value2 = 1;
+    for (let step = 1; step <= 4; ++step) {
         let isTrue = true;
-        console.log(lastPos1, lastPos2, value1, value2);
-        for (let i = firstPos1; isTrue == true; i += value1) {
+        for (let i = row; isTrue == true; i += value1) {
             let isSafe = true, isNull = 0;
-            for (let j = firstPos2; isSafe == true; j += value2) {
-                if (step % 2 != 0) {
-                    if (matrix[i][j] == 0) { 
-                        ++isNull;   
-                        cellsResult(i, j);
-                        neighboursValues(matrix, i, j);
-                        if (j == lastPos2) {
-                            isSafe = false;
-                        }
-                    } else {
+            for (let j = column; isSafe == true; j += value2) {
+                if (matrix[i][j] == 0) { 
+                    ++isNull;   
+                    cellsResult(i, j);
+                    neighboursValues(matrix, i, j);
+                    if (j == lastCol) {
                         isSafe = false;
                     }
                 } else {
-                    if (matrix[j][i] == 0) { 
-                        ++isNull;   
-                        cellsResult(j, i);
-                        neighboursValues(matrix, j, i);
-                        if (j == lastPos2) {
-                            isSafe = false;
-                        }
-                    } else {
-                        isSafe = false;
-                    }
+                    isSafe = false;
                 }
             }
-            if (i == lastPos1) {
+            if (i == lastRow) {
                 isTrue = false;
             }
             if (isNull == 0) {
                 isTrue = false;
             } 
         }
-        if (step % 2 == 0 && step % 4 != 0) {
-            value1 = -1;
-            lastPos1= 0;
-        } else {
-            let aux1 = firstPos1;
-            firstPos1 = firstPos2;
-            firstPos2 = aux1;
-            let aux2 = lastPos1;
-            lastPos1 = lastPos2;
-            lastPos2 = aux2;
-            let aux3 = value1;
-            value1 = value2;
-            value2 = aux3;
-        }
-        /*let isTrue2 = true;
+        let isTrue2 = true;
         for (let j = column; isTrue2 == true; j += value2) {
             let isSafe = true, isNull = 0;
             for (let i = row; isSafe == true; i += value1) {
@@ -172,7 +144,16 @@ function visitNeighbours(matrix, row, column) {
             if (isNull == 0) {
                 isTrue2 = false;
             }                
-        }*/
+        }
+        if (step % 2 == 0) {
+            value1 = -1;
+            lastRow = 0;
+            value2 = 1;
+            lastCol = 8;
+        } else {
+            value2 = -1;
+            lastCol = 0;
+        }
     }
 }
 
